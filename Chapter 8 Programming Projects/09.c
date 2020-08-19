aec21d3db17a35876d96c5a7e954714e129ea36d
Eddy-M-K <Eddy.M.K@outlook.com>
2020-08-18 23:28:00 -0700

    Added projects I've been working on

--- Chapter 8 Programming Projects/09.c
+++ Chapter 8 Programming Projects/09.c
@@ -1,17 +1,48 @@
+//Generates a "random walk" across a 10 * 10 array with the elements labelled with the letters A through Z
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) {
-  int row, column;
-  char ch, random_walk[10][10] = {{'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                                  {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                                  {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                                  {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                                  {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                                  {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                                  {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                                  {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                                  {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                                  {'.', '.', '.', '.', '.', '.', '.', '.', '.'}};
+  int row, column, direction;
+  char ch = 'A', random_walk[10][10] = {{'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}};
+  srand((unsigned) time(NULL));
+
+  row = rand() % 10;
+  column = rand() % 10;
+  random_walk[row][column] = ch;
+
+  for (; ch < 'Z' && ((random_walk[row + 1][column] == '.' && row + 1 <= 9) || (random_walk[row - 1][column] == '.' && row - 1 >= 0) || (random_walk[row][column + 1] == '.' && column + 1 <= 9) || (random_walk[row][column - 1] == '.' && column - 1 >= 0)); direction = rand() % 4) {
+    if (direction == 0 && random_walk[row + 1][column] == '.' && row + 1 <= 9) {
+      random_walk[++row][column] = ++ch;
+    }
+    else if (direction == 1 && random_walk[row - 1][column] == '.' && row - 1 >= 0) {
+      random_walk[--row][column] = ++ch;
+    }
+    else if (direction == 2 && random_walk[row][column + 1] == '.' && column + 1 <= 9) {
+      random_walk[row][++column] = ++ch;
+    }
+    else if (direction == 3 && random_walk[row][column - 1] == '.' && column - 1 >= 0) {
+      random_walk[row][--column] = ++ch;
+    }
+  }
+
+  for (row = 0; row < 10; row++) {
+    for (column = 0; column < 10; column++) {
+      printf("%c ", random_walk[row][column]);
+    }
+    printf("\n");
+  }
 
   return 0;
 }
---
+++ Chapter 8 Programming Projects/10.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void) {
+  int h24, m24, h12, m12 total_minutes, departure[] = {480, 583, 679, 767, 840, 945, 1140, 1305}, arrival[] = {616, 712, 811, 900, 968, 1075, 1280, 1438};
+  char d_aorp, a_aorp;
+
+  printf("Enter a 24-hour time: ");
+  scanf("%d :%d", &h24, &m24);
+
+  total_minutes = h24 * 60 + m24;
+
+  for (h24 = 0; h24 <= 7; h24++) {
+    if (minutes <= (departure[h24 + 1] - departure[h24]) / 2) {
+      if (departure[h24] >= 720) {
+        h12 = (departure[h24] - 720) / 60;
+        m12 = (departure[h24] - 720) % 60;
+        d_aorp = 'p';
+      }
+      else {
+        h12 = departure[h24] / 60;
+        m12 = departure[h24] % 60;
+        d_aorp = 'a';
+      }
+
+      if (arrival[h24] >= 720) {
+        a_aorp = 'p';
+      }
+      else a_aorp = 'a';
+
+      break;
+    }
+  }
+
+  printf("Closest departure time is %d:%2d %c.m., arriving at %d:%2d %c.m.", h12, m12, d_aorp, h12, m12, a_aorp);
+
+  return 0;
+}
