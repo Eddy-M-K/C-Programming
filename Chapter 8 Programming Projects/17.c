eb90cf4b5a5655e0e3dce8e33eb9d829b92b313a
Eddy-M-K <Eddy.M.K@outlook.com>
2020-08-24 00:17:50 -0700

    Modified 17.c

--- Chapter 8 Programming Projects/17.c
+++ Chapter 8 Programming Projects/17.c
@@ -4,18 +4,35 @@
 
 int main(void)
 {
-  int row, column;
+  int row, column, i, j;
 
   printf("This program creates a magic square of a specified size.\nThe size must be an odd number between 1 and 99.\nEnter size of magic square: ");
   scanf("%d", &i);
 
-  int magic_square[i][i] = {0};
+  int magic_square[i][i];
 
-  magic_square[0][i/2 + 1] = 1;
-
-  for (j = 1; j <= i * i; j++) {
-    for (row = 0, column = i/2 + 1;; row = ((row + i) % i) - 1), {
-
+  for (row = 0, column = i/2 + 1, j = 1; j <= i * i; row -= 1, column = (column + 1) % i, j++) {
+    if (row == -1) {
+      row = i - 1;
+    }
+    if (magic_square[row][column] == 0) {
+      magic_square[row][column] = j;
+    }
+    else {
+      row = (row + 1) % j;
+      magic_square[row][column] = j;
     }
   }
+
+  for (row = 0; row <= i; row++) {
+    for (column = 0; column <= i; column++) {
+      printf("%5d", magic_square[row][column]);
+
+      if (column == i) {
+        printf("\n");
+      }
+    }
+  }
+
+  return 0;
 }
