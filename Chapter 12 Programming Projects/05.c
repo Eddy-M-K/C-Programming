166df207e7b13b3693215345a50dcd85e2b0383f
Eddy Kim <Eddy.M.K@outlook.com>
2021-08-29 21:54:57 -0700

    Added part of 05.c

---
+++ Chapter 12 Programming Projects/05.c
@@ -0,0 +1,60 @@
+/*********************************
+ * Reverses the words in a       *
+ * sentence but with the use of  *
+ * pointers                      *
+ *                               *
+ *********************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 100
+
+char *get_message_end(char *array);
+void print_reversal(char *end, char *beginning);
+
+int main(void) 
+{
+  char message[N];
+  char *array = message;
+  char *end = NULL;
+
+  end = get_message_end(array);
+  print_reversal(end, array);
+
+}
+
+char *get_message_end(char *array) 
+{
+  char *end = array, ch = '\0';
+
+  printf("Enter a sentence: ");
+
+  for (; end != array + N; end++) {
+    ch = getchar();
+
+    if ((ch == '.') || (ch == '?') || (ch == '!')) {
+      break;
+    } else *end = ch;
+  } 
+
+  return end;
+}
+
+void print_reversal(char *end, char *beginning)
+{
+  char terminator = *end--, ;
+
+  for (; end != beginning; end--) {
+    if (*end == ' ') putchar(*end)
+    else {
+      char *temp = end;
+
+      do {
+        --temp;
+      } while (*(temp-1) != ' ' || temp != beginning);
+
+      
+    }
+  }
+}
