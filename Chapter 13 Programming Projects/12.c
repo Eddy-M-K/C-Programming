74a839f713f82c4adb6d9b3a9fcbd9d7877a9bd8
Eddy Kim <Eddy.M.K@outlook.com>
2021-09-16 16:57:20 -0700

    Completed Chapter 13/12.c

--- Chapter 13 Programming Projects/12.c
+++ Chapter 13 Programming Projects/12.c
@@ -13,52 +13,45 @@
 #define WORD_LEN 20
 #define SENT_LEN 30
 
-int prompt(char *sentence[]);
-void reverse(char *sentence[], int words);
-void print(char *sentence[], int words);
+int prompt(char sentence[][WORD_LEN+1]);
+void print_reverse(char sentence[][WORD_LEN+1], int words);
 
 int main(void)
 {
-  char *sentence[WORD_LEN+1] = (char [SENT_LEN][WORD_LEN]) {0}; 
+  char sentence[SENT_LEN][WORD_LEN+1] =  {0}; 
   int words = 0;
 
   words = prompt(sentence);
-  reverse(sentence, words);
-  print(sentence, words);
+  print_reverse(sentence, words);
 
   exit(EXIT_SUCCESS);
 }
 
-int prompt(char *sentence[])
+
+int prompt(char sentence[][WORD_LEN+1])
 {
   int words = 0;
   char current = 0;
 
   printf("Enter a sentence: ");
 
-  for (; words < SENT_LEN; words++) {
+  while (words < SENT_LEN) {
     for (int j = 0; j < WORD_LEN; j++)  {
-      if (current = getchar() == '\n') break;
-
-      sentence[words][j] = current;
+      current = getchar();
+      if (current == '\n' || current == ' ') break;
+      else sentence[words][j] = current;
     }
+    words++;
+
+    if (current == '\n')  break;
   }
 
   return words;
 }
 
-void reverse(char *sentence[], int words)
+void print_reverse(char sentence[][WORD_LEN+1], int words)
 {
-  for (int i = 0, j = words; i < j; i++, j--) {
-    char temp[WORD_LEN+1] = sentence[i];
-    strcpy(sentence[i], sentence[j]);
-    strcpy(sentence[j], temp);
-  }
-}
-
-void print(char *sentence[], int words)
-{
-  for (int i = 0; i < words; i++) {
+  for (int i = words - 1; i >= 0; i--) {
     printf("%s ", sentence[i]);
   }
 }
