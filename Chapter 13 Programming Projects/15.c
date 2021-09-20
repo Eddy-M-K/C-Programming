3fc19dc4a0cb5cc3b5010aac1d0a3f72a29c2713
Eddy Kim <Eddy.M.K@outlook.com>
2021-09-20 09:01:17 -0700

    Completed Chapter 13/15.c

--- Chapter 13 Programming Projects/15.c
+++ Chapter 13 Programming Projects/15.c
@@ -29,19 +29,77 @@ int main(void)
   char expression[EXPR_LEN + 1] = {0};
   int val = 0;
 
-  printf("Enter an RPN expression: ");
-  fgets(expression, EXPR_LEN + 1, stdin);
-
-  evaluate_RPN_expression(expression);
+  do {
+    printf("Enter an RPN expression: ");
+    fgets(expression, EXPR_LEN + 1, stdin);
+    val = evaluate_RPN_expression(expression);
+    make_empty(); 
+    printf("Value of expression: %d\n", val);
+  } while (expression[0] != 'q');
 
   exit(EXIT_SUCCESS);
 }
 
 int evaluate_RPN_expression(const char *expression)
 {
-  for (; *expression != '\0' && *expression != '='; expression++) {
+  for (; *expression != '\0'; expression++) {
     if (*expression == ' ') continue;
     else if (*expression == 'q') exit(EXIT_SUCCESS);
-    else if ()
+    else if (*expression >= '0' && *expression <= '9') {
+      push(*expression - '0');
+    } else {
+      switch(*expression) {
+        case '+':
+          push(pop() + pop());
+          break;
+        case '-': ;
+          int negative = -pop();
+          push(pop() + negative);
+          break;
+        case '*':
+          push(pop() * pop());
+          break;
+        case '/': ;
+          int divisor = pop();
+          push(pop() / divisor);
+          break;
+        case '=':          
+          return contents[--top];
+          break;
+      }
+    }
   }
+
+  return 1;
+}
+
+void make_empty(void)
+{
+  top = 0;
+}
+
+bool is_empty(void)
+{
+  return (top == 0);
+}
+
+bool is_full(void)
+{
+  return (top == STACK_SIZE);
+}
+
+void push(char operand)
+{
+  if (is_full()) {
+    printf("Expression is too complex");
+    exit(EXIT_FAILURE);
+  } else contents[top++] = operand;
+}
+
+char pop(void)
+{
+  if (top < 1) {
+    printf("Not enough operands in expression");
+    exit(EXIT_FAILURE);
+  } else return contents[--top];
 }
